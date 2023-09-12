d32e7932d32a8d6807ccee71ad800953f15c40cc
VY-1 <107969625+VY-1@users.noreply.github.com>
2023-09-12 00:57:05 +0000

    Updated search functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -34,24 +35,70 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+# ------------------------------- SEARCH ----------------------------------- #
+def get_login_info():
+    website = website_entry.get()
+    try:
+
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No database file found, add a record to create one")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            #Clear email and password entries
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+            #Load email and password from data.json to email and password entry fields
+            email_entry.insert(0, email)
+            password_entry.insert(0, password)
+            messagebox.showinfo(title=website, message=f"Email/Username: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(message="No login entry found for Website")
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data= {
+        website: {
+            "email": email,
+            "password": password
+
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showerror(title="Oops!", message="Please don't leave any filed empty!")
+        messagebox.showerror(title="Invalid inputs!", message="Please don't leave any filed empty!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+
+                #Reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -76,23 +123,26 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entry
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(sticky=W, row=1, column=1)
 website_entry.focus()   #start of with the cursor in the website_entry field
 
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=30)
+email_entry.grid(sticky=W, row=2, column=1)
 email_entry.insert(0, "username@domain.com")
 
-password_entry = Entry(width=35)
-password_entry.grid(row=3, column=1, columnspan=2)
+password_entry = Entry(width=30)
+password_entry.grid(sticky=W, row=3, column=1)
 
 #Button
-password_button = Button(text="Generate Password", command=generate_password)
-password_button.grid(row=3, column=2)
+search_button = Button(text="Search", width=15, command=get_login_info)
+search_button.grid(sticky=W, row=1, column=2)
 
-add_button = Button(text="Add", width=30, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+password_button = Button(text="Generate Password", width=15, command=generate_password)
+password_button.grid(sticky=W, row=3, column=2)
+
+add_button = Button(text="Add/Update", width=15, command=save)
+add_button.grid(sticky=W, row=4, column=2)
 
 
 
